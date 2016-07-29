605842a9ec9330f2582f44419ffe5a35693f1f89
Dominic Marcello <dmarce504@gmail.com>
2016-07-29 11:20:15 -0500

    inlined all of multipole.cpp

--- src/multipole.hpp
+++ src/multipole.hpp
@@ -51,4 +51,89 @@ public:
 	~multipole();
 };
 
+
+inline multipole::multipole() {
+}
+
+inline real multipole::operator ()() const {
+	return (*this)[0];
+}
+
+inline real& multipole::operator ()() {
+	return (*this)[0];
+}
+
+inline real multipole::operator ()(integer i, integer j) const {
+	return (*this)[1 + map2[i][j]];
+}
+
+inline real& multipole::operator ()(integer i, integer j) {
+	return (*this)[1 + map2[i][j]];
+}
+
+#ifdef CORRECTION_ON
+inline real multipole::operator ()(integer i, integer j, integer k) const {
+	return (*this)[7 + map3[i][j][k]];
+}
+
+inline real& multipole::operator ()(integer i, integer j, integer k) {
+	return (*this)[7 + map3[i][j][k]];
+}
+#endif
+
+inline multipole& multipole::operator =(const multipole& expansion) {
+	for (integer i = 0; i < MP; i++) {
+		(*this)[i] = expansion[i];
+	}
+	return *this;
+}
+
+inline multipole& multipole::operator =(real expansion) {
+	for (integer i = 0; i < MP; i++) {
+		(*this)[i] = expansion;
+	}
+	return *this;
+}
+
+inline multipole multipole::operator>>(const space_vector& dX) const {
+	multipole you = *this;
+	you >>= dX;
+	return you;
+}
+
+inline std::array<real, MP>& multipole::operator +=(const std::array<real, MP>& vec) {
+	for (integer i = 0; i < MP; i++) {
+		(*this)[i] += vec[i];
+	}
+	return *this;
+}
+
+inline multipole::~multipole() {
+}
+
+
+inline multipole& multipole::operator>>=(const space_vector& Y) {
+	multipole& me = *this;
+#ifdef CORRECTION_ON
+	for (integer p = 0; p < 3; p++) {
+		for (integer q = p; q < 3; q++) {
+			for (integer r = q; r < 3; r++) {
+				me(p, q, r) += me() * Y[p] * Y[q] * Y[r];
+				me(p, q, r) += Y[p] * me(r, q);
+				me(p, q, r) += Y[q] * me(p, r);
+				me(p, q, r) += Y[r] * me(q, p);
+			}
+		}
+	}
+#endif
+	for (integer p = 0; p < 3; p++) {
+		for (integer q = p; q < 3; q++) {
+			me(p, q) += me() * Y[p] * Y[q];
+		}
+	}
+	return me;
+}
+
+
+
 #endif /* multipole_H_ */
